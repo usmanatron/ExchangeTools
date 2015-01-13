89f40d0bdbe82a161372dfe53226ac4af101598b
Usman Iqbal <usman.iqbal@softwire.com>
2015-01-13 22:19:19 +0000

    Add configurable ApplicationSettings class (not all fields currently added)

---
+++ ExchangeOofScheduler.Core/ApplicationSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+
+namespace ExchangeOofScheduler.Core
+{
+  public class ApplicationSettings
+  {
+    private static NameValueCollection applicationSettings;
+
+    public ApplicationSettings()
+    {
+      applicationSettings = ConfigurationManager.GetSection("OutOfOfficeSettings") as NameValueCollection;
+    }
+
+    public string userEmail
+    {
+      get
+      {
+        return applicationSettings["userEmail"];
+      }
+    }
+
+    public bool debugModeEnabled
+    {
+      get { return bool.Parse(applicationSettings["debugModeEnabled"]); }
+    }
+  }
+}
