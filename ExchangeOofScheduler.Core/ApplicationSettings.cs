84027aedc6e2c025177859b7fd05d34223c3ce8e
Usman Iqbal <usman.iqbal@softwire.com>
2015-01-14 13:19:38 +0000

    Add more application settings

--- ExchangeOofScheduler.Core/ApplicationSettings.cs
+++ ExchangeOofScheduler.Core/ApplicationSettings.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections.Specialized;
+﻿using ExchangeOofScheduler.Core.Dates;
+using System;
+using System.Collections.Specialized;
 using System.Configuration;
+using Microsoft.Exchange.WebServices.Data;
 
 namespace ExchangeOofScheduler.Core
 {
   public class ApplicationSettings
   {
-    private static NameValueCollection applicationSettings;
+    private NameValueCollection applicationSettings;
+    private readonly DayOfWeekReader DayOfWeekReader;
+    private readonly DateRangeCalculator dateRangeCalculator;
 
-    public ApplicationSettings()
+    public ApplicationSettings(DayOfWeekReader dayOfWeekReader, DateRangeCalculator dateRangeCalculator)
     {
       applicationSettings = ConfigurationManager.GetSection("OutOfOfficeSettings") as NameValueCollection;
+      this.DayOfWeekReader = dayOfWeekReader;
+      this.dateRangeCalculator = dateRangeCalculator;
     }
 
     public string userEmail
@@ -23,6 +30,54 @@ namespace ExchangeOofScheduler.Core
     public bool debugModeEnabled
     {
       get { return bool.Parse(applicationSettings["debugModeEnabled"]); }
+    }
+
+    public DayOfWeek startDay
+    {
+      get { return DayOfWeekReader.Read(applicationSettings["startDay"]); }
+    }
+
+    public DayOfWeek endDay
+    {
+      get { return DayOfWeekReader.Read(applicationSettings["endDay"]); }
+    }
+
+    public DateRange nextApplicableDateRangeForOof
+    {
+      get { return dateRangeCalculator.CalculateNextDateRangeForOof(startDay, endDay); }
+    }
+
+    public string internalReply
+    {
+      get { return applicationSettings["internalReply"]; }
+    }
+
+    public OofExternalAudience sendToExternalRecipients
+    {
+      get
+      {
+        var configValue = applicationSettings["sendToExternalRecipients"].ToLower();
+        switch (configValue)
+        {
+          case "none":
+            return OofExternalAudience.None;
+          case "known":
+            return OofExternalAudience.Known;
+          case "all":
+            return OofExternalAudience.All;
+          default:
+            var message =
+              string.Format(
+                "Unexpected value for \"sendToExternalRecipients\" ({0}). Expected values are None,All and Known.",
+                configValue);
+            throw new ArgumentException(message);
+        }
+      }
+    }
+
+    public string externalReply
+    {
+      get { return applicationSettings["externalReply"]; }
     }
   }
 }
