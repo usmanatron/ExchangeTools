3d222c26b9225b6696bbcb90c197d1f5550e31bd
Usman Iqbal <usman.iqbal@softwire.com>
2015-01-14 22:45:56 +0000

    Rewrite DateRange Tests

--- ExchangeOofScheduler.Core.Tests/Dates/DateRangeTests.cs
+++ ExchangeOofScheduler.Core.Tests/Dates/DateRangeTests.cs
@@ -7,16 +7,54 @@ namespace ExchangeOofScheduler.Core.Tests.Dates
   [TestFixture]
   class DateRangeTests
   {
-    private static readonly DateTime wayPastDate = DateTime.Now.AddDays(-7);
-    private static readonly DateTime pastDate = DateTime.Now.AddDays(-1);
-    private static readonly DateTime futureDate = DateTime.Now.AddDays(1);
-    private static readonly DateTime wayFutureDate = DateTime.Now.AddDays(7);
+    private DateTime wayPastDate;
+    private DateTime pastDate;
+    private DateTime now;
+    private DateTime futureDate;
+    private DateTime wayFutureDate;
+
+    private object[] nonIntersectingDateRanges;
+    private object[] intersectingDateRanges;
+    // These date ranges are the wrong way around
+    private object[] exceptioningDateRanges;
+
+    [SetUp]
+    public void Setup()
+    {
+      wayPastDate = DateTime.Now.AddDays(-7);
+      pastDate = DateTime.Now.AddDays(-1);
+      now = DateTime.Now;
+      futureDate = DateTime.Now.AddDays(1);
+      wayFutureDate = DateTime.Now.AddDays(7);
+
+      nonIntersectingDateRanges = new object[]
+                                  {
+                                    new Tuple<DateTime, DateTime>(wayPastDate, pastDate),
+                                    new Tuple<DateTime, DateTime>(futureDate, wayFutureDate)
+                                  };
+      intersectingDateRanges = new object[]
+                               {
+                                 new Tuple<DateTime, DateTime>(wayPastDate, futureDate),
+                                 new Tuple<DateTime, DateTime>(wayPastDate, wayFutureDate),
+                                 new Tuple<DateTime, DateTime>(pastDate, futureDate),
+                                 new Tuple<DateTime, DateTime>(pastDate, wayFutureDate)
+                               };
+      exceptioningDateRanges = new object[]
+                               {
+                                 new Tuple<DateTime, DateTime>(wayFutureDate, futureDate),
+                                 new Tuple<DateTime, DateTime>(wayFutureDate, pastDate),
+                                 new Tuple<DateTime, DateTime>(wayFutureDate, wayPastDate),
+                                 new Tuple<DateTime, DateTime>(futureDate, pastDate),
+                                 new Tuple<DateTime, DateTime>(futureDate, wayPastDate),
+                                 new Tuple<DateTime, DateTime>(pastDate, wayPastDate)
+                               };
+    }
 
     [Test]
     [TestCaseSource("nonIntersectingDateRanges")]
     public void RangeWhichDoesNotIntersectNow_IsntHappeningNow(Tuple<DateTime, DateTime> range)
     {
-      var dateRange = new DateRange(range.Item1, range.Item2);
+      var dateRange = new DateRange(range.Item1, range.Item2, now);
       Assert.False(dateRange.HappeningNow);
     }
 
@@ -24,7 +62,7 @@ namespace ExchangeOofScheduler.Core.Tests.Dates
     [TestCaseSource("intersectingDateRanges")]
     public void RangeWhichDoesIntersectNow_HappeningNow(Tuple<DateTime, DateTime> range)
     {
-      var dateRange = new DateRange(range.Item1, range.Item2);
+      var dateRange = new DateRange(range.Item1, range.Item2, now);
       Assert.True(dateRange.HappeningNow);
     }
 
@@ -32,32 +70,7 @@ namespace ExchangeOofScheduler.Core.Tests.Dates
     [TestCaseSource("exceptioningDateRanges")]
     public void RangeWithDatesInDescendingOrder_Throws(Tuple<DateTime, DateTime> range)
     {
-      Assert.Throws<ArgumentException>(() => new DateRange(range.Item1, range.Item2));
+      Assert.Throws<ArgumentException>(() => new DateRange(range.Item1, range.Item2, now));
     }
-
-    private readonly object[] nonIntersectingDateRanges =
-    {
-      new Tuple<DateTime, DateTime>(wayPastDate, pastDate),
-      new Tuple<DateTime, DateTime>(futureDate, wayFutureDate)
-    };
-
-    private readonly object[] intersectingDateRanges =
-    {
-      new Tuple<DateTime, DateTime>(wayPastDate, futureDate),
-      new Tuple<DateTime, DateTime>(wayPastDate, wayFutureDate),
-      new Tuple<DateTime, DateTime>(pastDate, futureDate),
-      new Tuple<DateTime, DateTime>(pastDate, wayFutureDate)
-    };
-
-    // These date ranges are the wrong way around
-    private readonly object[] exceptioningDateRanges=
-    {
-      new Tuple<DateTime, DateTime>(wayFutureDate, futureDate),
-      new Tuple<DateTime, DateTime>(wayFutureDate, pastDate),
-      new Tuple<DateTime, DateTime>(wayFutureDate, wayPastDate),
-      new Tuple<DateTime, DateTime>(futureDate, pastDate),
-      new Tuple<DateTime, DateTime>(futureDate, wayPastDate),
-      new Tuple<DateTime, DateTime>(pastDate, wayPastDate)
-    };
   }
 }
