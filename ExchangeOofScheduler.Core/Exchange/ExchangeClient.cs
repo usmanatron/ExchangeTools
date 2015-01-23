21f836d8714e1dbf141d5d3beaf81bfc9004c9e0
Usman Iqbal <usman.iqbal@softwire.com>
2015-01-23 19:20:43 +0000

    Remove ExchangeServiceBuilder

--- ExchangeOofScheduler.Core/Exchange/ExchangeClient.cs
+++ ExchangeOofScheduler.Core/Exchange/ExchangeClient.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Exchange.WebServices.Data;
+using System;
 
 namespace ExchangeOofScheduler.Core.Exchange
 {
@@ -7,13 +8,35 @@ namespace ExchangeOofScheduler.Core.Exchange
     private readonly ExchangeService exchangeService;
     private readonly IApplicationSettings settings;
 
-    public ExchangeClient(ExchangeServiceBuilder exchangeServiceBuilder, IApplicationSettings settings)
+    public ExchangeClient(IApplicationSettings settings)
     {
       this.settings = settings;
-      this.exchangeService = exchangeServiceBuilder
-        .WithEmail(settings.userEmail)
-        .WithTracing(settings.debugModeEnabled)
-        .Build();
+      this.exchangeService = BuildService();
+    }
+
+    private ExchangeService BuildService()
+    {
+      var service = new ExchangeService { UseDefaultCredentials = true };
+
+      if (this.settings.debugModeEnabled)
+      {
+        service.TraceEnabled = true;
+        service.TraceFlags = TraceFlags.All;
+      }
+
+      service.AutodiscoverUrl(this.settings.userEmail, ValidateRedirectionUrl);
+      return service;
+    }
+
+    /// <summary>
+    /// Validate the contents of the redirection URL. In this simple validation
+    /// callback, the redirection URL is considered valid if it is using HTTPS
+    /// to encrypt the authentication credentials.
+    /// </summary>
+    private bool ValidateRedirectionUrl(string redirectionUrl)
+    {
+      var redirectionUri = new Uri(redirectionUrl);
+      return redirectionUri.Scheme == "https";
     }
 
     public OofSettings GetOofSettings()
