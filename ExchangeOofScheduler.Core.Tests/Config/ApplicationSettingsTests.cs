e59442ffc6feed727f0840f85cb86f635a814c66
Usman Iqbal <usman.iqbal@softwire.com>
2015-01-25 18:48:34 +0000

    ApplicationSettings tests

---
+++ ExchangeOofScheduler.Core.Tests/Config/ApplicationSettingsTests.cs
@@ -0,0 +1,70 @@
+﻿using ExchangeOofScheduler.Core.Config;
+using FakeItEasy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace ExchangeOofScheduler.Core.Tests.Config
+{
+  [TestFixture]
+  internal class ApplicationSettingsTests
+  {
+    private static IDictionary<string, string> config;
+    private static ApplicationSettings applicationSettings;
+
+    [SetUp]
+    public void Setup()
+    {
+      var configuration = A.Fake<IConfiguration>();
+      config = A.Fake<IDictionary<string, string>>();
+      A.CallTo(() => configuration.Config).Returns(config);
+
+      applicationSettings = new ApplicationSettings(configuration);
+    }
+
+    [Test]
+    [TestCaseSource("settingsTestCases")]
+    public void Settings_RetrievedFromExpectedMembers(StringSettingsTestCase testCase)
+    {
+      A.CallTo(testCase.FakeCall).Returns(testCase.Value);
+
+      var actualValue = testCase.CheckFunc.Invoke();
+
+      Assert.AreEqual(testCase.Value, actualValue);
+    }
+
+    [Test]
+    [TestCase("true", true)]
+    [TestCase("True", true)]
+    [TestCase("false", false)]
+    [TestCase("False", false)]
+    public void DebugModeSetting_ReadCorrectly(string setting, bool expectedValue)
+    {
+      A.CallTo(() => config["DebugModeEnabled"]).Returns(setting);
+
+      var actualValue = applicationSettings.DebugModeEnabled;
+
+      Assert.AreEqual(expectedValue, actualValue);
+    }
+
+    [Test]
+    [TestCase("")]
+    public void InvalidDebugModeSetting_Throws(string setting)
+    {
+      A.CallTo(() => config["DebugModeEnabled"]).Returns(setting);
+
+      Assert.Throws<FormatException>(() => { var a = applicationSettings.DebugModeEnabled; });
+    }
+
+    private readonly object[] settingsTestCases =
+      {
+        new StringSettingsTestCase(() => config["UserEmail"], "user@email.com", () => applicationSettings.UserEmail),
+        new StringSettingsTestCase(() => config["StartDay"], "We", () => applicationSettings.StartDay),
+        new StringSettingsTestCase(() => config["EndDay"], "Fr", () => applicationSettings.EndDay),
+        new StringSettingsTestCase(() => config["BoundaryTime"], "17:30:00", () => applicationSettings.BoundaryTime),
+        new StringSettingsTestCase(() => config["InternalReply"], "Internal Reply", () => applicationSettings.InternalReply),
+        new StringSettingsTestCase(() => config["ExternalReply"], "External Reply", () => applicationSettings.ExternalReply),
+        new StringSettingsTestCase(() => config["SendToExternalRecipients"], "Known", () => applicationSettings.SendToExternalRecipients)
+      };
+  }
+}
---
+++ ExchangeOofScheduler.Core.Tests/Config/StringSettingsTestCase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExchangeOofScheduler.Core.Tests.Config
+{
+  internal class StringSettingsTestCase
+  {
+    public Expression<Func<string>> FakeCall;
+    public string Value;
+    public Func<string> CheckFunc;
+
+    public StringSettingsTestCase(Expression<Func<string>> fakeCall, string value, Func<string> checkFunc)
+    {
+      this.FakeCall = fakeCall;
+      this.Value = value;
+      this.CheckFunc = checkFunc;
+    }
+  }
+}
